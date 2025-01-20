a7a2935f7296e52265604ad33b8d5c85ff16dfc2
林桂涛 <852951042@qq.com>
2025-01-20 14:46:19 +0800

    feat: javascript 的基本语法

---
+++ javaScript/01_JavaScript的基本语法/js/bing.js
@@ -0,0 +1,5 @@
+const bingEl = document.querySelector('.bing')
+
+bingEl.onclick = function () {
+  alert('bing 一下')
+}
---
+++ javaScript/01_JavaScript的基本语法/js/document-desc.js
@@ -0,0 +1,8 @@
+/**
+ * 和某人打招呼
+ * @param {string} name 姓名
+ * @param {number} age 年龄
+ */
+function sayHelloFn(name, age) {}
+
+sayHelloFn('sean', 18)
