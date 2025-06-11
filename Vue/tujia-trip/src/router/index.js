fd635fd7413d39dc9db5c6c2a3fa59caeb8c3061
林桂涛 <852951042@qq.com>
2025-06-11 14:51:07 +0800

    feat: vue-tujia-trip 城市列表功能

--- Vue/tujia-trip/src/router/index.js
+++ Vue/tujia-trip/src/router/index.js
@@ -22,6 +22,10 @@ const router = createRouter({
     {
       path: '/message',
       component: () => import('@/views/message/index.vue')
+    },
+    {
+      path: '/city',
+      component: () => import('@/views/city/index.vue')
     }
   ]
 })
